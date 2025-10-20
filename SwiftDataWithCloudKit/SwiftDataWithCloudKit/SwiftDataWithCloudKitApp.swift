2a163890d5380a1a6d06534d13d2ad50ce054a50
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2025-10-20 11:50:12 +0600

    init project

---
+++ SwiftDataWithCloudKit/SwiftDataWithCloudKit/SwiftDataWithCloudKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataWithCloudKitApp.swift
+//  SwiftDataWithCloudKit
+//
+//  Created by Sajid Shanta on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataWithCloudKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
